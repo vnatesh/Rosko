4dfb157f324dd3acbca2a2352284e12ed18a28e9
vnatesh <vikas.natesh@gmail.com>
2022-03-25 16:47:17 -0400

    end-to-end dnn inference experiment

---
+++ experiments/arm/end_to_end/rop_sgemm_test.cpp
@@ -0,0 +1,103 @@
+#include "cake.h"
+#include <string.h>
+
+
+
+int main(int argc, char* argv[]) {
+
+	int M, K, N, p, nz = 0;
+	float tmp;
+	struct timespec start, end;
+	double diff_t;
+
+	p = 4; // 4 cores on rasbpi 4
+
+	FILE* fp = fopen(argv[1],"r");
+	if(!fp) {
+		printf("Error: Could not open file\n");
+		exit(1);
+	}
+
+	char line[100];
+	int i = 0;
+	fgets(line, 100, fp);
+	strtok(line, " ");
+	N = atoi(strtok(NULL, " "));
+	strtok(NULL, " ");
+	K = atoi(strtok(NULL, " "));
+	strtok(NULL, " ");
+	M = atoi(strtok(NULL, " "));
+
+	printf("%d %d %d \n",M,K,N );
+
+	fgets(line, 100, fp);
+	fgets(line, 100, fp);
+	fgets(line, 100, fp);
+
+
+	float* A = (float*) malloc(M * K * sizeof( float ));
+
+	char* pEnd;
+	fgets(line, 100, fp);
+	tmp = strtof(line, &pEnd);
+
+	while(fgets(line, 100, fp)) {
+
+		if(tmp != 0) {
+			nz++;
+		}
+
+		A[i] = tmp;
+		i++;
+
+		tmp = strtof(line, NULL);
+		
+	}
+
+	if(tmp != 0) {
+		nz++;
+	}
+
+	A[i] = tmp;
+
+	fclose(fp);
+
+	printf("M = %d K = %d N = %d nz = %d\n", M, K, N, nz);
+
+
+
+
+	float* B = (float*) malloc(K * N * sizeof( float ));
+	float* C = (float*) calloc(M * N , sizeof( float ));
+
+	// initialize B
+    srand(time(NULL));
+	rand_init(B, K, N);
+
+	cake_cntx_t* cake_cntx = cake_query_cntx();
+	
+	double ret1 = 0.0, ret2 = 0.0;
+	int iters = 10;
+
+	for(int i = 0; i < iters; i++) {
+		ret1 += cake_sp_sgemm(A, B, C, M, N, K, p, cake_cntx);
+		ret2 += cake_sgemm(A, B, C, M, N, K, p, cake_cntx);
+	}
+
+    char fname[50];
+    snprintf(fname, sizeof(fname), "result_end_to_end");
+    FILE *fp1;
+    fp1 = fopen(fname, "a");
+    fprintf(fp1, "rosko,%d,%d,%d,%d,%f\n",M,K,N,nz,ret1 / iters);
+    fprintf(fp1, "cake,%d,%d,%d,%d,%f\n",M,K,N,nz,ret2 / iters);
+    fclose(fp1);
+
+	// cake_sgemm_checker(A, B, C, N, M, K);
+	
+	free(A);
+	free(B);
+	free(C);
+
+	return 0; 
+}
+
---
+++ experiments/arm/end_to_end/run.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Run matmul bench on raspberry pi 3b for 1..4 cores through linux perf
+
+# compile arm_test with ARMPL
+gcc -I/opt/arm/armpl_21.1_gcc-9.3/include -fopenmp  arm_test.c -o test.o  /opt/arm/armpl_21.1_gcc-9.3/lib/libarmpl_lp64_mp.a  -L{ARMPL_DIR}/lib -lm -o arm_test;
+
+# compile ARMCL sgemm test (NEON)
+export ARMCL_PATH=/home/ubuntu/ComputeLibrary;
+export LD_LIBRARY_PATH=$ARMCL_PATH/build:$LD_LIBRARY_PATH
+
+aarch64-linux-gnu-g++ -o neon_sgemm.o -c -Wall -DARCH_ARM -Wextra -pedantic \
+-Wdisabled-optimization -Wformat=2 -Winit-self -Wstrict-overflow=2 -Wswitch-default \
+-std=c++14 -Woverloaded-virtual -Wformat-security -Wctor-dtor-privacy -Wsign-promo \
+-Weffc++ -Wno-overlength-strings -Wlogical-op -Wnoexcept -Wstrict-null-sentinel -C \
+-fopenmp -march=armv8-a -DENABLE_NEON -DARM_COMPUTE_ENABLE_NEON -Wno-ignored-attributes \
+-DENABLE_FP16_KERNELS -DENABLE_FP32_KERNELS -DENABLE_QASYMM8_KERNELS \
+-DENABLE_QASYMM8_SIGNED_KERNELS -DENABLE_QSYMM16_KERNELS -DENABLE_INTEGER_KERNELS \
+-DENABLE_NHWC_KERNELS -DENABLE_NCHW_KERNELS -O3 -D_GLIBCXX_USE_NANOSLEEP \
+-DARM_COMPUTE_CPP_SCHEDULER=1 -DARM_COMPUTE_OPENMP_SCHEDULER=1 \
+-DARM_COMPUTE_GRAPH_ENABLED -DARM_COMPUTE_CPU_ENABLED \
+-I$ARMCL_PATH/include -I$ARMCL_PATH -I$ARMCL_PATH neon_sgemm.cpp
+
+aarch64-linux-gnu-g++ -o neon_sgemm -fopenmp \
+neon_sgemm.o $ARMCL_PATH/build/utils/Utils.o -L$ARMCL_PATH/build \
+-L$ARMCL_PATH -lpthread -larm_compute -larm_compute_core
+
+# compile rop_sgemm_test
+make;
+
+
+for file in matrices_93/*.mtx; 
+do
+
+	./rop_sgemm_test $file; 
+	./arm_test $file; 
+	./neon_sgemm  $file;
+
+done
+# run matmul bench
+
+
+# python3 plots.py $NTRIALS; 
+
