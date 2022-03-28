1345a3997632e145ca8351158796cab7139e1316
vnatesh <vikas.natesh@gmail.com>
2022-03-28 10:14:37 -0400

    arm ablation experiment

---
+++ experiments/arm/ablation/rosko_sgemm_test.cpp
@@ -0,0 +1,63 @@
+#include "cake.h"
+
+
+
+
+int main( int argc, char** argv ) {
+	 // run_tests();
+
+    if(argc < 3) {
+        printf("Enter M, K, and N\n");
+        exit(1);
+    }
+
+	int M, K, N, p, sp;
+	struct timespec start, end;
+	double diff_t;
+
+	M = atoi(argv[1]);
+	K = atoi(argv[2]);
+	N = atoi(argv[3]);
+	p = atoi(argv[4]);
+	sp = atoi(argv[5]);
+
+	printf("M = %d, K = %d, N = %d, cores = %d, 
+		sparsity = %f\n", M,K,N,p, ((float) sp) / 100.0);
+
+	float* A = (float*) malloc(M * K * sizeof( float ));
+	float* B = (float*) malloc(K * N * sizeof( float ));
+	float* C = (float*) calloc(M * N , sizeof( float ));
+
+	// initialize A and B
+    srand(time(NULL));
+	rand_sparse(A, M, K, ((float) sp) / 100.0);
+	// rand_sparse_gaussian(A, M, K, 0, 1);
+	// rand_init(A, M, K);
+	// print_array(A, M*K);
+	// exit(1);
+	rand_init(B, K, N);
+
+	cake_cntx_t* cake_cntx = cake_query_cntx();
+	
+	
+	clock_gettime(CLOCK_REALTIME, &start);
+
+	double ret = cake_sp_sgemm(A, B, C, M, N, K, p, cake_cntx);
+
+    clock_gettime(CLOCK_REALTIME, &end);
+    long seconds = end.tv_sec - start.tv_sec;
+    long nanoseconds = end.tv_nsec - start.tv_nsec;
+    diff_t = seconds + nanoseconds*1e-9;
+	printf("sp_sgemm time: %f \n", ret); 
+
+	// cake_sgemm_checker(A, B, C, N, M, K);
+	
+	free(A);
+	free(B);
+	free(C);
+
+	return 0;
+}
+
+
+
