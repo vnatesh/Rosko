1678bef73b4896792ba5f1e24c23a36ab1488a3c
vnatesh <vikas.natesh@gmail.com>
2022-06-09 04:01:04 -0400

    collect L1 misses from DRAM using PMU event and new DRAM BW plot

--- experiments/amd/sparse/plots.py
+++ experiments/amd/sparse/plots.py
@@ -10,33 +10,69 @@ from matplotlib import ticker as mticker
 
 
 
+
+
 def plot_rosko_vs_aocl_sparse(fname = 'rosko_vs_aocl_sp'):
 	plt.rcParams.update({'font.size': 12})
 	# all matrices used are 99.87-99.97% sparse
-	labels = ['Fash_mnist', \
-	'har','indianpines','J_VowelsSmall', \
-	'kmnist','mnist_test','optdigits',\
-	'usps','worms20']
+	# labels = ['Fash_mnist', \
+	# 'har','indianpines','J_VowelsSmall', \
+	# 'kmnist','mnist_test','optdigits',\
+	# 'usps','worms20']
 	df1 = pandas.read_csv('bar_load')
+	labels = [i[:-4] for i in df1[df1['algo'] == 'aocl']['file']._values]
 	rel_tput = df1[df1['algo'] == 'aocl']['time']._values / df1[df1['algo'] == 'rosko']['time']._values
-	rel_tput = rel_tput[1:]
 	X = np.arange(len(labels))
 	#
 	plt.figure(figsize = (6,5))
-	plt.title('(b) Throughput of SpMM in Rosko vs aocl', fontsize = 18)
+	plt.title('(a) Throughput of SpMM in Rosko vs aocl', fontsize = 18)
 	plt.bar(X + 0.00, rel_tput, color = 'r', width = 0.25)
-	plt.bar(X + 0.25, 9*[1], color = intel_color, width = 0.25)
+	plt.bar(X + 0.25, 9*[1], color = 'g', width = 0.25)
 	plt.xticks(X, labels, fontsize = 18)
 	plt.xticks(rotation=60)
 	plt.ylabel("Tput relative to aocl", fontsize = 16)
 	plt.yticks(np.arange(0, 5, 1), fontsize = 16)
-	plt.legend(labels=['Rosko', 'aocl'])
+	plt.legend(labels=['Rosko', 'AOCL'])
 	plt.tight_layout()
-	# plt.savefig("%s_perf.pdf" % fname , bbox_inches='tight')
+	plt.savefig("%s_perf.pdf" % fname , bbox_inches='tight')
+	plt.show()
+	plt.clf()
+	plt.close('all')
+	#
+	#
+	dram_bw_rosko = []
+	dram_bw_aocl = []
+	#
+	for i in df1[df1['algo'] == 'aocl']['file']._values:
+		a = open("reports_amd/report_rosko_%s.csv" % i ,'r').read()
+		data = [q for q in a.split('\n') if 'PID' in q][0].split(',')
+		cpu_time = df1[(df1['algo'] == 'rosko') & (df1['file'] == i)]['time']._values.mean()
+		dram_bw_rosko.append((((float(data[2]) + float(data[3]))*50000*64) / cpu_time) / 1e9)
+		#
+		a = open("reports_amd/report_aocl_%s.csv" % i ,'r').read()
+		data = [q for q in a.split('\n') if 'PID' in q][0].split(',')
+		cpu_time = df1[(df1['algo'] == 'aocl') & (df1['file'] == i)]['time']._values.mean()
+		dram_bw_aocl.append((((float(data[2]) + float(data[3]))*50000*64) / cpu_time) / 1e9)
+	#
+	#
+	X = np.arange(len(labels))
+	plt.figure(figsize = (6,5))
+	plt.title('(b) DRAM BW of SpMM in Rosko vs AOCL', fontsize = 18)
+	# plt.tick_params(labelbottom=False)  
+	plt.bar(X + 0.00, dram_bw_rosko, color = 'r', width = 0.25)
+	plt.bar(X + 0.25, dram_bw_aocl, color = 'g', width = 0.25)
+	plt.ylabel("DRAM Bw (GB/sec)", fontsize = 16)
+	plt.xticks(X, labels, fontsize = 18)
+	plt.xticks(rotation=60)
+	plt.yticks( fontsize = 16)
+	plt.legend(labels=['Rosko', 'AOCL'])
+	plt.tight_layout()
+	plt.savefig("%s_dram.pdf" % fname , bbox_inches='tight')
 	plt.show()
 	plt.clf()
 	plt.close('all')
 
 
 plot_rosko_vs_aocl_sparse()
+
 
--- experiments/amd/sparse/sparse_prof.sh
+++ experiments/amd/sparse/sparse_prof.sh
@@ -7,7 +7,7 @@ pip install ssgetpy
 
 python3 - <<END
 import ssgetpy
-ssgetpy.search(rowbounds=(5000,45000),colbounds=(5000,45000), \
+ssgetpy.search(rowbounds=(5000,25000),colbounds=(5000,25000), \
     dtype = 'real', group='ML_Graph').download(destpath = '.', extract=True)
 END
 
@@ -16,8 +16,8 @@ mv **/*.mtx .
 rm *label*;
 
 
-# export OMP_NUM_THREADS=24 
-# export GOMP_CPU_AFFINITY="0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23"         
+# export OMP_NUM_THREADS=24
+# export GOMP_CPU_AFFINITY="0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23"        
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$PWD/aocl-sparse/build/release/library
 NTRIALS=10
 
@@ -26,18 +26,43 @@ NTRIALS=10
 make
 
 echo "algo,file,p,time" >> bar_load
+mkdir reports;
 
-for file in *.mtx; 
+for file in *.mtx;
 do
 
     ./aoclsparse-bench --function=csrmm --precision=d --alpha=1 --beta=0 --iters=$NTRIALS --mtx=$file > out;
 value=$file python3 - <<END
 import os
 open('bar_load', 'a').write("aocl,%s,1,%s\n" % (os.environ["value"], \
-float(open('out', 'r').read().split('\n')[2].split()[10]) / 1000.0)) 
+float(open('out', 'r').read().split('\n')[2].split()[10]) / 1000.0))
 END
     rm out;
     ./cake_spgemm_test $file $NTRIALS;
 
-done
 
+    /home/vinatesh/AMDuProf_Linux_x64_3.5.671/bin/AMDuProfCLI collect \
+    --event event=pmcx043,umask=0x08,interval=50000 \
+    --event event=pmcx043,umask=0x40,interval=50000 \
+    --event event=timer -o /tmp/test \
+    ./cake_spgemm_test $file 1;
+
+    /home/vinatesh/AMDuProf_Linux_x64_3.5.671/bin/AMDuProfCLI report -i /tmp/test/*
+
+    mv /tmp/test/*/report.csv reports/report_rosko_$file.csv;
+    rm -rf /tmp/test;
+
+    
+
+    /home/vinatesh/AMDuProf_Linux_x64_3.5.671/bin/AMDuProfCLI collect \
+    --event event=pmcx043,umask=0x08,interval=50000 \
+    --event event=pmcx043,umask=0x40,interval=50000 \
+    --event event=timer -o /tmp/test \
+    ./aoclsparse-bench --function=csrmm --precision=d --alpha=1 --beta=0 --iters=1 --mtx=$file
+
+    /home/vinatesh/AMDuProf_Linux_x64_3.5.671/bin/AMDuProfCLI report -i /tmp/test/*
+
+    mv /tmp/test/*/report.csv reports/report_aocl_$file.csv;
+    rm -rf /tmp/test;    
+
+done
