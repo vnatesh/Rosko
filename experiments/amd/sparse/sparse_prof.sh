fc6cd605084786fdfabb77255c29fcfba436596c
vnatesh <vikas.natesh@gmail.com>
2022-07-08 09:08:14 -0400

    update exp

--- experiments/amd/sparse/sparse_prof.sh
+++ experiments/amd/sparse/sparse_prof.sh
@@ -6,6 +6,7 @@
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$PWD/aocl-sparse/build/release/library
 NTRIALS=10
 
+export GOMP_CPU_AFFINITY="0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23";
 
 # compile cake_sp_sgemm_test
 make
@@ -13,41 +14,44 @@ make
 echo "algo,file,p,time" >> bar_load
 mkdir reports;
 
-for file in *.mtx;
+for file in data/*.mtx;
 do
 
-    ./aoclsparse-bench --function=csrmm --precision=d --alpha=1 --beta=0 --iters=$NTRIALS --mtx=$file > out;
-value=$file python3 - <<END
+    ./aoclsparse-bench --function=csrmm --precision=d --alpha=1 --beta=0 --iters=$NTRIALS --mtx=${file##*/} > out;
+value=${file##*/} python3 - <<END
 import os
 open('bar_load', 'a').write("aocl,%s,1,%s\n" % (os.environ["value"], \
 float(open('out', 'r').read().split('\n')[2].split()[10]) / 1000.0))
 END
     rm out;
-    ./cake_spgemm_test $file $NTRIALS;
+    ./cake_spgemm_test ${file##*/} $NTRIALS;
 
 
-    /home/vinatesh/AMDuProf_Linux_x64_3.5.671/bin/AMDuProfCLI collect \
+    # /home/vinatesh/AMDuProf_Linux_x64_3.5.671/bin/AMDuProfCLI collect \
+    AMDuProfCLI collect \
     --event event=pmcx043,umask=0x08,interval=50000 \
     --event event=pmcx043,umask=0x40,interval=50000 \
     --event event=timer -o /tmp/test \
-    ./cake_spgemm_test $file 1;
+    ./cake_spgemm_test ${file##*/} 1;
 
-    /home/vinatesh/AMDuProf_Linux_x64_3.5.671/bin/AMDuProfCLI report -i /tmp/test/*
+    # /home/vinatesh/AMDuProf_Linux_x64_3.5.671/bin/AMDuProfCLI report -i /tmp/test/*
+    AMDuProfCLI report -i /tmp/test/*
 
-    mv /tmp/test/*/report.csv reports/report_rosko_$file.csv;
+    mv /tmp/test/*.csv reports/report_rosko_${file##*/}.csv;
     rm -rf /tmp/test;
 
     
-
-    /home/vinatesh/AMDuProf_Linux_x64_3.5.671/bin/AMDuProfCLI collect \
+    # /home/vinatesh/AMDuProf_Linux_x64_3.5.671/bin/AMDuProfCLI collect \
+    AMDuProfCLI collect \
     --event event=pmcx043,umask=0x08,interval=50000 \
     --event event=pmcx043,umask=0x40,interval=50000 \
     --event event=timer -o /tmp/test \
-    ./aoclsparse-bench --function=csrmm --precision=d --alpha=1 --beta=0 --iters=1 --mtx=$file
+    ./aoclsparse-bench --function=csrmm --precision=d --alpha=1 --beta=0 --iters=1 --mtx=${file##*/}
 
-    /home/vinatesh/AMDuProf_Linux_x64_3.5.671/bin/AMDuProfCLI report -i /tmp/test/*
+    # /home/vinatesh/AMDuProf_Linux_x64_3.5.671/bin/AMDuProfCLI report -i /tmp/test/*
+    AMDuProfCLI report -i /tmp/test/*
 
-    mv /tmp/test/*/report.csv reports/report_aocl_$file.csv;
+    mv /tmp/test/*.csv reports/report_aocl_${file##*/}.csv;
     rm -rf /tmp/test;    
 
 done
