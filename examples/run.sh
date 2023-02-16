78b493a6a3c824513144bc01931e0424b29c0e05
vnatesh <vikas.natesh@gmail.com>
2023-02-16 13:47:41 -0500

    mkl csr sparse test

---
+++ examples/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export GOMP_CPU_AFFINITY="0 1 2 3 4 5 6 7 8 9";
+
+x=$PWD
+sudo cp mkl_sparse_gemm.cpp /opt/intel/oneapi/mkl/2021.1.1/examples/sycl/spblas
+cd /opt/intel/oneapi/mkl/2021.1.1/examples/sycl
+make sointel64 examples="spblas/mkl_sparse_gemm" sycl_devices=cpu
+cp _results/intel64_so_tbb/spblas/mkl_sparse_gemm.out $x
+cd $x
